d23cf09b6d5b1763c8bee342c5f3ebfd544b15dc
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-10-08 22:52:52 +0200

    ref: UploadAvatar decorator

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -6,14 +6,12 @@ import {
   Req,
   UploadedFile,
   UseGuards,
-  UseInterceptors,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { Routes, SwaggerApiTags } from 'src/utils/constants';
 import { ProfilesService } from './profiles.service';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { avatarStorage } from './storage';
+import { UploadAvatar } from './decorators/upload-avatar.decorator';
 
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
@@ -23,46 +21,22 @@ export class ProfilesController {
   constructor(private readonly profilesService: ProfilesService) {}
 
   @Post('/upload-avatar')
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        file: {
-          type: 'string',
-          format: 'binary',
-        },
-      },
-    },
-  })
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: avatarStorage,
-    }),
-  )
-  async uploadAvatar(@Req() req, @UploadedFile() file: Express.Multer.File) {
+  @UploadAvatar()
+  async uploadAvatar(
+    @Req() req,
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
     return this.profilesService.saveAvatar(req.user.uuid, file.filename);
   }
 
   @Patch('/update-avatar')
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        file: {
-          type: 'string',
-          format: 'binary',
-        },
-      },
-    },
-  })
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: avatarStorage,
-    }),
-  )
-  async updateAvatar(@Req() req, @UploadedFile() file: Express.Multer.File) {
+  @UploadAvatar()
+  async updateAvatar(
+    @Req() req,
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
     return this.profilesService.updateAvatar(req.user.uuid, file.filename);
   }
 
@@ -71,3 +45,15 @@ export class ProfilesController {
     return this.profilesService.removeAvatar(req.user.uuid);
   }
 }
+
+// new ParseFilePipeBuilder()
+// .addFileTypeValidator({
+//   fileType: new RegExp('.(png|jpeg|jpg)'),
+// })
+// .addMaxSizeValidator({
+//   message: 'Please upload a file less than 1MB',
+//   maxSize: 1024 * 1024 * 1,
+// })
+// .build({
+//   errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+// }),
